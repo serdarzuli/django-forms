fa70bdcc0887e5ce895813e4d5b5b364fce112c6
ZuliSerdar <serdar.zuli@gmail.com>
2025-02-18 14:56:11 +0100

    Created new field as dept regarding select departments

--- home/models.py
+++ home/models.py
@@ -7,4 +7,14 @@ class Contact(models.Model):
     message = models.TextField()
     
     
+# coktan secmeli bir kismimiz var departmant diye, ona buraya bir karsilik ENUMORATIN yazicaz. ayrica bunu ingilziceye cevir
+
+    DEPARTMENTS = [
+        ('MF', 'Manufacturing'),
+        ('SH', 'Shipping'),
+        ('AD', 'Administration'),
+        ('HR', 'Human Resources')
+    ]
+    
+    dept = models.CharField(max_length=2, choices=DEPARTMENTS, default=DEPARTMENTS[3][0])
     
